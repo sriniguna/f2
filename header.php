9fb9920bb0193b26284f2bc89d2831027f9b202b
Srini Guna <s@srinig.com>
2015-04-29 11:52:23 +0530

    Removed the now dropped hgroup element in favour of div.site-branding

--- header.php
+++ header.php
@@ -27,11 +27,11 @@
 <div id="page" class="hfeed site">
 	<?php do_action( 'before' ); ?>
 	<header id="masthead" class="site-header" role="banner">
-		<hgroup>
+		<div class="site-branding">
 			<h1 class="site-title"><a href="<?php echo home_url( '/' ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 			<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
 			<?php f2_logo_image(); ?>
-		</hgroup>
+		</div><!-- .site-branding -->
 
 		<?php if( f2_get_option( 'hide_header_nav' ) != 'on' ) : ?>
 		<nav role="navigation" class="site-navigation main-navigation">
